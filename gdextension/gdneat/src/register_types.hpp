eb48485155ce764bc19ad25608540bc6a15830e1
Mumbly Juergens <mumblyjuergens@gmail.com>
2026-09-20 21:56:15 +1000

    refactor: gdneat moving to own repo

--- gdextension/gdneat/src/register_types.hpp
+++
@@ -1,4 +0,0 @@
-#include <godot_cpp/core/class_db.hpp>
-
-void initialize_gdneat_module(godot::ModuleInitializationLevel level);
-void uninitialize_gdneat_module(godot::ModuleInitializationLevel level);
